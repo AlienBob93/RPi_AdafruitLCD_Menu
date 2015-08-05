f1a8379bb5809dc50342bf148335a297bd44fdc4
Prashant Rao <prashantrao420@gmail.com>
2015-08-05 16:20:32 -0400

    Update testMenu.py
    Added weather and radio menus:
    Weather menu shows current weather of specified place
    network shows current IP address and SSID
    radio option doesn't seem to be working..

--- testMenu.py
+++ testMenu.py
@@ -1,34 +1,43 @@
-#Daniel Juenger, github.com/sleeepyjack
+#Prashant Rao 
 
+#import pywapi as weather
+import string 
 from time import sleep
-from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
-from menu import Menu
+import Adafruit_CharLCD as LCD
+from Menu import Menu
 
-lcd = Adafruit_CharLCDPlate()
+lcd = LCD.Adafruit_CharLCDPlate()
 menu = Menu()
 
-#The menu can show strings, bash and python expressions
+#loc_ID = pywapi.get_location_ids('mckees rock, pa')
+#print(loc_ID)
 
+#The menu can show strings, bash and python expressions
 #		     topElement(      Name , Type of content , Lower row content)
 
-top1 = menu.topElement("< Netzwerk     >", "STRING", "        v")
-top2 = menu.topElement("< System       >", "STRING", "        v")
-top3 = menu.topElement("< top3         >", "STRING", "        v")
-top4 = menu.topElement("< top4         >", "STRING", "        v")
-top5 = menu.topElement("< top5         >", "STRING", "        v")
-sub11 = menu.subElement("Netzw.>Signal", "BASH", "iwconfig wlan0 | awk -F'[ =]+' '/Signal level/ {print $7}' | cut -d/ -f1")
-sub12 = menu.subElement("Netzw.>SSID", "BASH", "iwconfig wlan0 | grep 'ESSID:' | awk '{print $4}' | sed 's/ESSID://g'")
-sub13 = menu.subElement("Netzw.>Internet", "BASH", "ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && echo ok || echo error")
-sub21 = menu.subElement("System>CPU", "PYTHON", 'str(str(psutil.cpu_percent()) + "%")')
-sub22 = menu.subElement("System>CPU-Temp.", "STRING", "TODO")
-sub23 = menu.subElement("System>RAM", "PYTHON", 'str(str(psutil.phymem_usage()[3])+"% used")')
+top1 = menu.topElement("< Weather      >", "STRING", "        v")
+top2 = menu.topElement("< Network     >", "STRING", "        v")
+top3 = menu.topElement("< System       >", "STRING", "        v")
+top4 = menu.topElement("< Radio        >", "STRING", "        v")
+
+
+sub11 = menu.subElement("Weather>Cond.", "PYTHON", 'str(Weather.currCond())')
+sub12 = menu.subElement("Weather>Temp.", "PYTHON", 'str(str(Weather.currTemp()) +chr(223)+ "F")')
+sub13 = menu.subElement("Weather>Loc.", "PYTHON", 'str(Weather.currLoc())')
+
+sub21 = menu.subElement("Netw.>Signal", "BASH", "iwconfig wlan0 | awk -F'[ =]+' '/Signal level/ {print $7}' | cut -d/ -f1")
+sub22 = menu.subElement("Netw.>SSID", "BASH", "iwconfig wlan0 | grep 'ESSID:' | awk '{print $4}' | sed 's/ESSID://g'")
+sub23 = menu.subElement("Netw>IP", "BASH", "ifconfig wlan0 | grep 'inet addr:' | awk '{print $2}' | sed 's/addr://g'")
+
+sub31 = menu.subElement("System>CPU", "PYTHON", 'str(str(psutil.cpu_percent()) + "%")')
+
+sub41 = menu.subElement("Radio>", "RADIO", 'str(radioINFO.station())')
 
 #Adding elements to the menu
 menu.addTopElement(top1)
 menu.addTopElement(top2)
 menu.addTopElement(top3)
 menu.addTopElement(top4)
-menu.addTopElement(top5)
 
 menu.addSubElement(top1, sub11)
 menu.addSubElement(top1, sub12)
@@ -36,12 +45,12 @@ menu.addSubElement(top1, sub13)
 menu.addSubElement(top2, sub21)
 menu.addSubElement(top2, sub22)
 menu.addSubElement(top2, sub23)
-
-color = lcd.TEAL
+menu.addSubElement(top3, sub31)
+menu.addSubElement(top4, sub41)
 
 #initializing display
 lcd.clear()
-lcd.backlight(color)
+lcd.set_color(1.0, 0, 0)
 
 #little loading animation
 i = 0
@@ -52,5 +61,4 @@ while(i < 16):
     i += 1
 
 #starting the menu
-menu.startMenu(lcd, color)
-
+menu.startMenu(lcd, LCD)
